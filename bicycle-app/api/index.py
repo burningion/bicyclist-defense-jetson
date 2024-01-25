ea310949224a9ca6a499722d86763bdaced6e81a
Kirk Kaiser <kirk@zothcorp.com>
2024-01-25 08:49:01 -0500

    Now maybe better?

--- bicycle-app/api/index.py
+++ bicycle-app/api/index.py
@@ -1,17 +1,34 @@
-from fastapi import FastAPI
+from fastapi import BackgroundTasks, FastAPI
 import subprocess
 
 import os
 
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
+def get_realsense_data():
+    subprocess.call(f"python3 record_30s.py --num-frames=300".split(" "))
+    os.remove("recording.lock")
+    print("recording finished")
+    return 
+
 @app.get("/api/py/healthcheck")
 def healthchecker():
     return {"status": "success", "message": "Integrated FastAPI Framework with Next.js successfully!"}
 
+@app.get("/api/py/recording")
+def recording():
+    if os.path.exists('recording.lock'):
+        return {"recording": True}
+    else:
+        return {"recording": False}
+
 @app.post("/api/py/record")
-def record():
-    print("recoding...")
-    print(f"current directory: {os.getcwd()}")
-    subprocess.call(f"python3 rrecord_30s.py --num-frames=300".split(" "))
-    return True
+def record(background_tasks: BackgroundTasks):
+    if os.path.exists('recording.lock'):
+        return {"message": "Already recording"}
+    else:
+        with open('recording.lock', 'w') as f:
+            pass
+        print("recording started")
+        background_tasks.add_task(get_realsense_data)
+        return {"message": "Recording started"}
--- bicycle-app/app/page.tsx
+++ bicycle-app/app/page.tsx
@@ -1,17 +1,35 @@
 'use client'
 import Image from "next/image";
 import { Button } from "@/app/components/Button";
+import {useState, useEffect} from "react";
 
-function record() {
-  fetch('/api/py/record', {
-    method: 'POST',
-  });
-}
 
 export default function Home() {
+  
+  const [recording, setRecording] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch('/api/py/recording', {
+        method: 'GET',
+      }).then((res) => {
+        res.json().then((data) => {
+          setRecording(data.recording);
+        });
+      });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  
+  function record() {
+    setRecording(true);
+    fetch('/api/py/record', {
+      method: 'POST',
+    });
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button className="px-12 py-7 text-9xl" onClick={record}>Record 30 seconds</Button>
+      <Button className="px-12 py-7 text-9xl" onClick={record} disabled={recording}>Record 30 seconds</Button>
     </main>
   );
 }
