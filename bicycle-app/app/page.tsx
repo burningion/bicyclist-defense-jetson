8b0170b2d1be6ce94f0a43779cb02f5ac06c15e6
Kirk Kaiser <kirk@zothcorp.com>
2024-03-25 14:14:42 -0400

    let's see how this works

--- bicycle-app/app/page.tsx
+++ bicycle-app/app/page.tsx
@@ -29,6 +29,8 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-9xl">Bicycle Recording App</h1>
+      <p>This application will allow you to record your cycling trips with <a href="https://www.rerun.io/">rerun</a> for later analysis.</p>
       <Button className="px-12 py-7 text-9xl" onClick={record} disabled={recording}>{recording? 'Recording' : 'Record 30 seconds'}</Button>
     </main>
   );
