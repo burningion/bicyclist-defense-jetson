c8ce898ed69a5f92f963cf19179dc25e83e619ad
Kirk Kaiser <kirk@zothcorp.com>
2024-03-25 15:07:32 -0400

    just testing for mobile

--- bicycle-app/app/page.jsx
+++ bicycle-app/app/page.jsx
@@ -61,7 +61,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-2xl">Bicycle Recording App</h1>
-      <div className="w-[14em]">
+      <div>
       <img id="camera_image" src="" alt="Camera Image"/>
       </div>
       <p>This application will allow you to record your cycling trips with <a href="https://www.rerun.io/">rerun</a> for later analysis.</p>
