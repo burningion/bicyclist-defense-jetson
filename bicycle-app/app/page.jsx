68a3b8b0be3a22f27f1a47dac5b6bad216293402
Kirk Kaiser <kirk@zothcorp.com>
2024-03-27 13:19:02 -0400

    one more style change

--- bicycle-app/app/page.jsx
+++ bicycle-app/app/page.jsx
@@ -64,7 +64,7 @@ export default function Home() {
       <div>
       <img id="camera_image" src="" alt="Camera Image"/>
       </div>
-      <button className="px-12 py-7 text-9xl" onClick={record} disabled={recording}>{recording? 'Recording' : 'Record 30 seconds'}</button>
+      <button className="px-12 py-7 text-xl bg-slate-800 text-slate-50" onClick={record} disabled={recording}>{recording? 'Recording' : 'Record 30 seconds'}</button>
       <p>This application will allow you to record your cycling trips with <a href="https://www.rerun.io/">rerun</a> for later analysis.</p>
     </main>
   );
