66e77bb29a9600300fc203ac49298c73f3ab2ac6
Kirk Kaiser <kirk@zothcorp.com>
2024-03-28 12:24:37 -0400

    I dont know why this might have broken

--- bicycle-app/app/page.jsx
+++ bicycle-app/app/page.jsx
@@ -62,12 +62,12 @@ export default function Home() {
   function record_video() {
     if (recording_video) {
       setRecordingVideo(false);
-      fetch('/api/py/stop-video', {
+      fetch('http://ubuntu:8000/api/py/stop-video', {
         method: 'POST',
       });
     } else {
       setRecordingVideo(true);
-      fetch('/api/py/record-video', {
+      fetch('http://ubuntu:8000/api/py/record-video', {
         method: 'POST',
       });
     }
